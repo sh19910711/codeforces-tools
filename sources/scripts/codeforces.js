534e1f0d2010cbaab3dd024ce355269c0a07b9bf
Hiroyuki Sano <sh19910711@gmail.com>
2012-10-16 13:02:44 +0900

    [codeforces.js] Codeforces.is_problem_page_url()を修正

--- sources/scripts/codeforces.js
+++ sources/scripts/codeforces.js
@@ -4,14 +4,15 @@
 (function(window, namespace, undefined) {
 	var REGEXP_PROBLEMSET_PROBLEM = /^[^:]+:\/\/[^\/]+\/problemset\/problem\/([0-9]+)\/([A-Z]+)\/?$/;
 	var REGEXP_CONTEST_PROBLEM = /^[^:]+:\/\/[^\/]+\/contest\/([0-9]+)\/problem\/([A-Z]+)\/?$/;
-	var self = {
+
+	var Codeforces = {
 		/**
 		 * URLから問題IDを取得する
 		 * 
 		 * @param url
 		 */
 		get_problem_id_from_url : function(url) {
-			if (!self.is_problem_page_url(url)) {
+			if (!Codeforces.is_problem_page_url(url)) {
 				return false;
 			}
 
@@ -30,7 +31,7 @@
 		 * @param url
 		 */
 		get_contest_id_from_url : function(url) {
-			if (!self.is_problem_page_url(url)) {
+			if (!Codeforces.is_problem_page_url(url)) {
 				return false;
 			}
 
@@ -49,15 +50,9 @@
 		 * @param url
 		 */
 		is_problem_page_url : function(url) {
-			// check valid
-			if (REGEXP_PROBLEMSET_PROBLEM.test(url)) {
-				return true;
-			}
-			if (REGEXP_CONTEST_PROBLEM.test(url)) {
-				return true;
-			}
-			return false;
+			return REGEXP_PROBLEMSET_PROBLEM.test(url) || REGEXP_CONTEST_PROBLEM.test(url);
 		}
 	};
-	window[namespace] = self;
+
+	window[namespace] = Codeforces;
 })(this, 'Codeforces');
